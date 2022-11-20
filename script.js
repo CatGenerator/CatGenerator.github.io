cf448d5a53ed9381553b5780d317ceb1053d9f86
CatGenerator <118480948+CatGenerator@users.noreply.github.com>
2022-11-20 02:11:25 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,21 +2,29 @@ let generate_btn = document.querySelector(".generate_btn");
 
 generate_btn.addEventListener("click", fetchPics);
 
+var catsImgUrl = ''
+var z = 0;
+
 function fetchPics() {
     let catsImgDiv = document.querySelector(".catsImgDiv")
     catsImgDiv.innerHTML = ''
     fetch('https://api.thecatapi.com/v1/images/search')
     .then(response => response.json())
     .then((data) => {
-        let catsImgUrl = data[0].url
+        catsImgUrl = data[0].url
         var imgWidth = data[0].width
-        var w = window.innerWidth;
-        console.log((w-imgWidth)/2)
-        
+        var imgHeight = data[0].height
+        var w = window.innerWidth
+        var h = window.innerHeight
+        while(imgWidth>w || imgHeight>h) {
+            imgWidth = imgWidth*0.5;
+            imgHeight = imgHeight*0.5;
+        }
 
         let catsImgEl = document.createElement("img")
         catsImgEl.setAttribute('src', `${catsImgUrl}`)
-        catsImgEl.classList.add("showcase")
+        catsImgEl.setAttribute('width', imgWidth)
+        catsImgEl.setAttribute('height', imgHeight)
         
         let catsImgDiv = document.querySelector(".catsImgDiv")
         catsImgDiv.appendChild(catsImgEl)
